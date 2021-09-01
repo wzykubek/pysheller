77f0559869fa582f93aad718483b140b01e2f97d
samedamci <samedamci@disroot.org>
2021-09-01 21:52:15 +0200

    Update example

--- example/__init__.py
+++ example/__init__.py
@@ -1,3 +1,10 @@
 from pysheller import PySheller
+from os import path
 
-PySheller(name="test-app", description="Very example app.", commands_dir="./commands")
+cmd_dir_path = path.dirname(path.realpath(__file__))
+
+PySheller(
+    name="test-app",
+    description="Very example app.",
+    commands_dir=path.join(cmd_dir_path, "commands"),
+)
